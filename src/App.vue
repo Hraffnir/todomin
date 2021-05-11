2d75c32290936368f6568a574f3529a8ed15ec64
Ryan Langman <ryan@wetu.com>
2021-05-11 20:40:52 +0200

    'Start'

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <h1>Test</h1>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import HelloWorld from './components/HelloWorld.vue'
+
+export default defineComponent({
+  name: 'Todomin (Pronounced "too-doo-muhn")',
+  components: {
+    HelloWorld
+  }
+})
+</script>
+
+<style lang="scss">
+@import "./assets/main.scss";
+
+body, html {
+  background: #D56CA5;
+}
+</style>
